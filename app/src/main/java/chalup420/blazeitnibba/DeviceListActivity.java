3407d5311f3f56ca164f649cff20cc1dde8c0ab2
WaterBlueNewWorld <tutureales@gmail.com>
2020-04-03 14:10:44 -0700

    First

---
+++ app/src/main/java/chalup420/blazeitnibba/DeviceListActivity.java
@@ -0,0 +1,112 @@
+package chalup420.blazeitnibba;
+
+import java.util.Set;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
+
+
+public class DeviceListActivity extends Activity {
+
+    private static final String TAG = "DeviceListActivity";
+    private static final boolean D = true;
+
+
+
+    Button tlbutton;
+    TextView textView1;
+
+
+    public static String EXTRA_DEVICE_ADDRESS = "device_address";
+
+    public BluetoothAdapter mBtAdapter;
+    public ArrayAdapter mPairedDevicesArrayAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.device_list);
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+
+        checkBTState();
+
+        textView1 = (TextView) findViewById(R.id.connecting);
+        textView1.setTextSize(40);
+        textView1.setText(" ");
+
+
+        mPairedDevicesArrayAdapter = new ArrayAdapter(this, R.layout.device_name);
+
+
+        ListView pairedListView = (ListView) findViewById(R.id.paired_devices);
+        pairedListView.setAdapter(mPairedDevicesArrayAdapter);
+        pairedListView.setOnItemClickListener(mDeviceClickListener);
+
+        // Get the local Bluetooth adapter
+        mBtAdapter = BluetoothAdapter.getDefaultAdapter();
+
+
+        Set pairedDevices = mBtAdapter.getBondedDevices();
+
+
+       /* if (pairedDevices.size() > 0) {
+            findViewById(R.id.title_paired_devices).setVisibility(View.VISIBLE);//make title viewable
+            for (BluetoothDevice bt : pairedDevices) {
+                mPairedDevicesArrayAdapter.add(bt.getName() + "\n" + bt.getAddress());
+
+            }
+
+        } else {
+            String noDevices = "Ningun dispositivo pudo ser emparejado";
+            mPairedDevicesArrayAdapter.add(noDevices);
+        } */
+    }
+
+
+    private OnItemClickListener mDeviceClickListener = new OnItemClickListener() {
+        public void onItemClick(AdapterView av, View v, int arg2, long arg3) {
+
+            textView1.setText("Conectando...");
+
+            String info = ((TextView) v).getText().toString();
+            String address = info.substring(info.length() - 17);
+
+
+            Intent i = new Intent(DeviceListActivity.this, MainActivity.class);
+            i.putExtra(EXTRA_DEVICE_ADDRESS, address);
+            startActivity(i);
+        }
+    };
+
+    private void checkBTState() {
+
+        mBtAdapter=BluetoothAdapter.getDefaultAdapter(); // CHECK THIS OUT THAT IT WORKS!!!
+        if(mBtAdapter==null) {
+            Toast.makeText(getBaseContext(), "El dispositivo no soporta Bluetooth", Toast.LENGTH_SHORT).show();
+        } else {
+            if (mBtAdapter.isEnabled()) {
+                Log.d(TAG, "...Bluetooth Activado...");
+            } else {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, 1);
+
+            }
+        }
+    }
+}
